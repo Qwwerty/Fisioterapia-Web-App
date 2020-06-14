4f6a14fab8e612e596435c9c53afb565c3fde091
qwwerty <verdinhoraro2009@hotmail.com>
2020-06-13 23:28:04 -0300

    corrigido o gitignore

---
+++ dist/precache-manifest.a84f5982acb6107f96ff36cbc4788b72.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd760d8a480501fc8e74",
+    "url": "/css/app.435b08a3.css"
+  },
+  {
+    "revision": "0883e53c42d99e34627f",
+    "url": "/css/chunk-vendors.494dc643.css"
+  },
+  {
+    "revision": "815104333698f655c7587ac7f23bf0a3",
+    "url": "/exercicios/exercicio1.png"
+  },
+  {
+    "revision": "59bc95c703e63c5db8ca3d6a6ae24b15",
+    "url": "/exercicios/exercicio13.png"
+  },
+  {
+    "revision": "157ff3ba2dccac5022d78ea25f376389",
+    "url": "/exercicios/exercicio2.png"
+  },
+  {
+    "revision": "950513fd29895b7cc475cd60e9408e63",
+    "url": "/exercicios/exercicio3.png"
+  },
+  {
+    "revision": "0ae813c12d8e022a63615b3822d634f3",
+    "url": "/exercicios/exercicio5.png"
+  },
+  {
+    "revision": "05de5861994052bdac6db1a6f9e7bc1a",
+    "url": "/exercicios/exercicio6.png"
+  },
+  {
+    "revision": "ed5ec150135bb0da0c7d88091aa7a7b1",
+    "url": "/exercicios/exercicio7.png"
+  },
+  {
+    "revision": "29bf400c344edea7a7e6083ec46262ef",
+    "url": "/exercicios/exercicio8.png"
+  },
+  {
+    "revision": "e9e4d38a6a48b6a55959f5e060acfbab",
+    "url": "/exercicios/exercicio9.png"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "4617c82739df64d82546263b6b118a95",
+    "url": "/img/paralisia.png"
+  },
+  {
+    "revision": "390baba6f89e3b28baaf4b956d11b7e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd760d8a480501fc8e74",
+    "url": "/js/app.781d1f23.js"
+  },
+  {
+    "revision": "0883e53c42d99e34627f",
+    "url": "/js/chunk-vendors.53cf1705.js"
+  },
+  {
+    "revision": "4b79cbb4eb72eed8864d65c765a19240",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
